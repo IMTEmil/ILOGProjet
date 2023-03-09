4cf4a899798043dd105fa7d182baaf063709c86e
Emil Pérouse <emil.perouse@etu.imt-nord-europe.fr>
2023-03-09 21:48:08 +0100

    add new effects

--- src/TestNative.java
+++ src/TestNative.java
@@ -8,7 +8,8 @@ public class TestNative {
 
     public static void main(String[] args)
     {
-        System.out.println(args[0]);
         CopyWAVFileAddEffect(args[0], 3, 0.5, 1);
+
+        CopyWAVFileAddEffect("o_" + args[0], 1, 0.5, 1);
     }
 }
